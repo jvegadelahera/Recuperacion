19add144cc0f8faf63102fb9787a9c00b8b7edaa
Edu <jvegadelahera@gmail.com>
2020-06-15 17:23:45 +0200

    Commit 2

--- src/ED_T2.java
+++ src/ED_T2.java
@@ -5,12 +5,12 @@ public class ED_T2 {
 			double [] dato = new double [3];
 			double fuerza ;
 			System.out.println("Introduce la masa del objeto 1: ");
-			dato [0] = utiles.leerDouble ();
+			dato [0] = utils.leerDouble ();
 			System.out.println("Introduce la masa del objeto 2: ");
-			dato [1] = utiles.leerDouble ();
+			dato [1] = utils.leerDouble ();
 			System.out.println("Introduce la distancia que los separa: ");
-			dato [2] = utiles.leerDouble ();
-			fuerza = 60.3 * (dato [0] * dato [1] / dato [2] * dato [2] );
+			dato [2] = utils.leerDouble ();
+			fuerza = 125.3 * (dato [0] * dato [1] / dato [2] * dato [2] );
 			System.out.println("El resultado del computo es el siguiente ");
 			System.out.println("La fuerza gravitacional entre los dos objetos");
 			System.out.println("Alcanza un valor de: + fuerza");
